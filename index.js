b81e93e570043c83ba84e291792f20c363bc5d6e
Anton Golub <antongolub@antongolub.com>
2022-07-11 17:05:43 +0300

    test: check bun-config option

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import _ from 'lodash'
 
 console.log(_.camelCase('--foo-bar'))
+
+try {
+  require('@antongolub/iso8601')
+  console.log('@antongolub/iso8601 is loaded')
+} catch (e) {
+  console.log('can not load dev dependency @antongolub/iso8601')
+}
